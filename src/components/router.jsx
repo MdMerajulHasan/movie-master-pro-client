65f48b5b4b7a51fe2907d18116686ce2f5ae119a
Jim <merajuljim@gmai.com>
2025-11-11 14:51:29 +0600

    required pages initialized

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,6 +1,25 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../layouts/Root";
+import Home from "../pages/Home";
+import AllMovies from "../pages/AllMovies";
+import AddMovie from "../pages/AddMovie";
+import MovieDetails from "../pages/MovieDetails";
+import UpdateMovie from "../pages/UpdateMovie";
+import MyCollection from "../pages/MyCollection";
 
-const router = createBrowserRouter([{ path: "/", Component: Root }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "/movies", element: <AllMovies></AllMovies> },
+      { path: "/movies/add", element: <AddMovie></AddMovie> },
+      { path: "/movies/:id", element: <MovieDetails></MovieDetails> },
+      { path: "/movies/update/:id", element: <UpdateMovie></UpdateMovie> },
+      { path: "/movies/my-collection", element: <MyCollection></MyCollection> },
+    ],
+  },
+]);
 
 export default router;
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import App from "../App";
+import { Outlet } from "react-router";
 
 const Root = () => {
   return (
     <div>
-      <App></App>
+      <Outlet></Outlet>
     </div>
   );
 };
