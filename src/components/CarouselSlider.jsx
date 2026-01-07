75be2f6cac5efe10c2d822be0269c6bd00260858
Jim <merajuljim@gmai.com>
2026-01-07 13:46:26 +0600

    carousel section edited

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const CarouselSlider = ({ movies }) => {
   return (
-    <div className="w-3/5 md:w-[45%] lg:w-[30%] mx-auto h-full">
+    <div className="w-3/5 md:w-[30%] lg:w-[25%] mx-auto h-2/3">
       <Carousel
         showIndicators={false}
         showThumbs={false}
@@ -13,7 +13,7 @@ const CarouselSlider = ({ movies }) => {
         infiniteLoop={true}
         interval={3000}
         showStatus={false}
-        className="md:h-[350px] lg:h-full"
+        className="md:h-full lg:h-full"
       >
         {movies.map((movie, index) => (
           <div key={index} className="md:h-[350px] lg:h-full">
