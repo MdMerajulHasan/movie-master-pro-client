28e0df3e22d8606a2b303caa286814231ef208bd
Jim <merajuljim@gmai.com>
2026-01-09 20:24:31 +0600

    search option added in all movies page, pagination added in all movies page

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -1,40 +1,89 @@
 import React, { use, useEffect, useState } from "react";
-import { useLoaderData } from "react-router";
 import MovieCard from "../components/MovieCard";
 import toast from "react-hot-toast";
 import { useSearchParams } from "react-router";
 import { AuthContext } from "../contexts/AuthContext";
 import Loader from "../components/Loader";
 
+const Limit = 10;
+
 const AllMovies = () => {
   const { loading, setLoading } = use(AuthContext);
   const [params] = useSearchParams();
   const genre = params.get("genre");
+
   const [moviesToShow, setMoviesToShow] = useState([]);
+  const [rating, setRating] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     setLoading(true);
+    if (rating) {
+      fetch(
+        `http://localhost:3000/movies?min=${rating}&genre=${
+          genre || ""
+        }&limit=${Limit}&skip=${currentPage * Limit}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setMoviesToShow(data.result);
+          const pages = Math.ceil(data.total / Limit);
+          setTotalPage(pages);
+          setRating(rating);
+          setLoading(false);
+          toast("Movies Filtered Successfully!");
+        })
+        .catch((err) => toast("Movies did not filtered: " + err.message));
+    } else {
+      const url = genre
+        ? `http://localhost:3000/movies?genre=${genre}&limit=${Limit}&skip=${
+            currentPage * Limit
+          }&search=${searchText || ""}`
+        : `http://localhost:3000/movies?limit=${Limit}&skip=${
+            currentPage * Limit
+          }&search=${searchText || ""}`;
 
-    const url = genre
-      ? `http://localhost:3000/movies?genre=${genre}`
-      : `http://localhost:3000/movies`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setMoviesToShow(data);
-        setLoading(false);
-      })
-      .catch((err) => toast(err.message));
-  }, [setLoading, genre]);
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setMoviesToShow(data.result);
+          const pages = Math.ceil(data.total / Limit);
+          setTotalPage(pages);
+          setLoading(false);
+        })
+        .catch((err) => toast(err.message));
+    }
+  }, [genre, setLoading, currentPage, rating, searchText]);
 
   const handleFilter = (rating) => {
-    fetch(`http://localhost:3000/movies?min=${rating}&genre=${genre}`)
+    setLoading(true);
+    fetch(
+      `http://localhost:3000/movies?min=${rating}&genre=${
+        genre || ""
+      }&limit=${Limit}&skip=${currentPage * Limit}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setMoviesToShow(data);
+        setMoviesToShow(data.result);
+        const pages = Math.ceil(data.total / Limit);
+        setTotalPage(pages);
+        setRating(rating);
+        setLoading(false);
         toast("Movies Filtered Successfully!");
       })
       .catch((err) => toast("Movies did not filtered: " + err.message));
+  };
+
+  const handleSearch = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearchDone = () => {
+    setSearchText(searchInput);
+    setCurrentPage(0);
   };
 
   if (loading) {
@@ -45,32 +94,82 @@ const AllMovies = () => {
       <h2 className="mb-1 md:mb-4 text-primary dark:text-white font-bold text-2xl text-center md:text-4xl lg:text-6xl">
         All Movies
       </h2>
-      <div className="grid grid-cols-5 md:grid-cols-5 lg:grid-cols-7 gap-1 w-11/12 mx-auto">
-        <div className="col-span-1">
-          <select
-            onChange={(e) => handleFilter(e.target.value)}
-            className="select text-primary dark:bg-slate-800 dark:text-white font-bold md:text-xs"
-          >
-            <option value="">Sort By Rating</option>
-            <option value="1">1-10</option>
-            <option value="2">2-10</option>
-            <option value="3">3-10</option>
-            <option value="4">4-10</option>
-            <option value="5">5-10</option>
-            <option value="6">6-10</option>
-            <option value="7">7-10</option>
-            <option value="8">8-10</option>
-            <option value="9">9-10</option>
-            <option value="10">10</option>
-          </select>
+      <div className="mb-1 md:mb-4 w-11/12 mx-auto flex flex-col md:flex-row gap-2 md:gap-0 md:justify-between items-center">
+        <div className="flex gap-1 items-center w-11/12 md:w-auto">
+          <label className="input text-primary border-primary dark:border-secondary dark:text-secondary">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" onChange={handleSearch} />
+          </label>
+          <button onClick={handleSearchDone} className="btn btn-secondary w-20">
+            Search
+          </button>
         </div>
-        <div className="col-span-4 md:col-span-4 lg:col-span-6">
-          {moviesToShow?.length ? (
-            <MovieCard movies={moviesToShow}></MovieCard>
-          ) : (
-            <p className="text-center text-primary font-bold text-base md:text-xl lg:text-3xl">
-              No movies in this rating range or genre!
-            </p>
+        <select
+          onChange={(e) => handleFilter(e.target.value)}
+          className="select w-11/12 md:w-auto border-primary dark:border-secondary text-primary dark:bg-slate-800 dark:text-white font-bold md:text-xs"
+        >
+          <option value="">Sort By Rating</option>
+          <option value="1">1-10</option>
+          <option value="2">2-10</option>
+          <option value="3">3-10</option>
+          <option value="4">4-10</option>
+          <option value="5">5-10</option>
+          <option value="6">6-10</option>
+          <option value="7">7-10</option>
+          <option value="8">8-10</option>
+          <option value="9">9-10</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      <div>
+        {moviesToShow?.length ? (
+          <MovieCard movies={moviesToShow}></MovieCard>
+        ) : (
+          <p className="text-center text-primary font-bold text-base md:text-xl lg:text-3xl">
+            No movies found!
+          </p>
+        )}
+        <div className="flex justify-center flex-wrap gap-3 py-10">
+          {currentPage > 0 && (
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              className="btn btn-secondary w-20"
+            >
+              Prev
+            </button>
+          )}
+
+          {[...Array(totalPage).keys()].map((i) => (
+            <button
+              key={i}
+              onClick={() => setCurrentPage(i)}
+              className={`btn ${i === currentPage && "btn-secondary"} w-20`}
+            >
+              {i + 1}
+            </button>
+          ))}
+          {currentPage < totalPage - 1 && (
+            <button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              className="btn btn-secondary w-20"
+            >
+              Next
+            </button>
           )}
         </div>
       </div>
